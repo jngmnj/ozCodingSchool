444fdf77df1bdd8f6304a8a9da72f7482bb64dd3
jngmnj <exorbitance@naver.com>
2025-04-09 11:37:00 +0900

    ✨ Feat: react router 설정 완료
    - Main, Detail, Search 페이지 생성
    - Layout 생성하여 Outlet적용
    - main.jsx, App.jsx react router 적용

--- React/animals-app/src/App.jsx
+++ React/animals-app/src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Layout from './Layout';
+import Detail from './page/Detail';
+import Main from './page/Main';
+import Search from './page/Search';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Routes>
+        <Route path="" element={<Layout />}>
+          <Route index element={<Main />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/search" element={<Search />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
---
+++ React/animals-app/src/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <>
+      <header>
+        <h1>💚 동물 조아 💚</h1>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>all rights reserved to OZ</footer>
+    </>
+  );
+}
+
+export default Layout
