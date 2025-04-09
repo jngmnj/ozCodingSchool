6b8e009ca15159617657533b7a3db5f30084b7e9
jngmnj <exorbitance@naver.com>
2025-04-09 13:53:00 +0900

    ✨ Feat: detail 페이지 구현
    - id를 useParams()로 받아 data에서 find()로 하나의 데이터 찾음
    - 상세정보 렌더링

--- React/animals-app/src/page/Detail.jsx
+++ React/animals-app/src/page/Detail.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { data } from "../assets/data/data";
 
 const Detail = () => {
-  return (
-    <div>Detail</div>
-  )
-}
+  const { id } = useParams();
+  console.log(id);
 
-export default Detail
+  const animalData = data.find((item) => item.id === Number(id));
+  console.log(animalData);
+  return (
+    <div className="detail">
+      <div className="img-box">
+        <img src={animalData.img} />
+      </div>
+      <div className="name">{animalData.name}</div>
+      <div className="desc">{animalData.description}</div>
+    </div>
+  );
+};
+
+export default Detail;
