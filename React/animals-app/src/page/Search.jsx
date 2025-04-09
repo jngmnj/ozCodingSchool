b6d32c2c17c1e262b7b3180da15585cbb9ce7309
jngmnj <exorbitance@naver.com>
2025-04-09 14:21:46 +0900

    ✨ 한국어 정규식으로 검색기능 구현
    - korean-regexp 패키지로 정규식검색(자음 모음만으로 검색가능)
    - useSearchParmas()로 쿼리스트링에서 검색어 추출
    - 검색어로 필터링된 filteredData 생성후 AnimalList에 전달

--- React/animals-app/src/page/Search.jsx
+++ React/animals-app/src/page/Search.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { getRegExp } from "korean-regexp";
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { data } from "../assets/data/data";
+import AnimalList from "../components/AnimalList";
 
 const Search = () => {
+    const [searchParams]= useSearchParams();
+    const param = searchParams.get("animal");
+    const reg = getRegExp(param);
+
+    const filteredData = data.filter((item) => item.name.match(reg));
+
   return (
-    <div>Search</div>
+    <AnimalList data={filteredData} />
   )
 }
 
