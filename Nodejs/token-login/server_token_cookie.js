5119018d68d643f4d0926f86518b036190603b19
jngmnj <exorbitance@naver.com>
2025-05-15 13:21:22 +0900

    ✨ Feat: Nodejs token 실습
    - 토큰생성후 쿠키에 저장하는 방법
    - 클라이언트측 변수에 저장하여 헤더로 요청하는 방법

---
+++ Nodejs/token-login/server_token_cookie.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+
+const users = [
+  {
+    user_id: "test",
+    password: "1234",
+    user_name: "테스트 유저",
+    user_info: "테스트 유저입니다.",
+  },
+];
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+    methods: ["OPTIONS", "GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+const secretKey = "secretTokenKey"; // JWT 비밀키
+
+app.get("/", (req, res) => {
+  const payload = jwt.verify(req.cookies.accessToken, secretKey);
+  if (!payload) {
+    return res.status(401).json({ success: false, message: "사용자 정보조회 실패" });
+  }
+  const userInfo = users.find((el) => el.user_id === payload.user_id);
+  return res.json(userInfo);
+});
+
+app.delete("/", (req, res) => {
+  res.clearCookie("accessToken");
+  return res.json({ success: true, message: "로그아웃 성공" });
+});
+
+app.post("/", (req, res) => {
+  const { userId, password } = req.body;
+  const userInfo = users.find(
+    (user) => user.user_id === userId && user.password === password
+  );
+
+  if (!userInfo) {
+    return res.status(401).json({ success: false, message: "로그인 실패" });
+  }
+  const acessToken = jwt.sign(
+    { user_id: userInfo.user_id },
+    secretKey,
+    { expiresIn: 1000 * 60 * 10 } // 10분
+  );
+  res.cookie("accessToken", acessToken);
+  return res.send("로그인 성공! 세션 생성 완료!");
+});
+
+app.listen(3000, () => {
+  console.log("서버실행 http://localhost:3000");
+});
